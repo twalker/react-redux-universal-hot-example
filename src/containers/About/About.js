501c99ad62b833147da9c583b33b545f984a7561
Daniel Oh <doh422@gmail.com>
2016-01-07 16:25:32 -0800

    added assessment div to HTML.js and Traitify UI js to About

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,50 +1,28 @@
 import React, {Component} from 'react';
-import DocumentMeta from 'react-document-meta';
-import { MiniInfoBar } from 'components';
-import config from '../../config';
 
 export default class About extends Component {
 
-  state = {
-    showKitten: false
-  }
+  // state = {
+  //   showKitten: false
+  // }
 
-  handleToggleKitten = () => this.setState({showKitten: !this.state.showKitten});
-
+  // handleToggleKitten = () => this.setState({showKitten: !this.state.showKitten});
   render() {
-    const {showKitten} = this.state;
-    const kitten = require('./kitten.jpg');
+    // const deckId = 'career_deck';
+    const assessmentId = 'a9ffbd96-ed29-4e7f-8dfb-fc6f867eb875';
+
+    Traitify.setPublicKey('a653qn1aosgiee1jv49haksgoc');
+    Traitify.setHost('api-sandbox.traitify.com');
+    Traitify.setVersion('v1');
+    // Traitify.setSecretKey('hemohtsgsqg85ai75i1ki84244');
+
+    console.log(assessmentId);
+
+    Traitify.ui.load(assessmentId, '.assessment');
+
     return (
       <div className="container">
         <h1>About Us</h1>
-        <DocumentMeta title={config.app.title + ': About Us'}/>
-
-        <p>This project was originally created by Erik Rasmussen
-          (<a href="https://twitter.com/erikras" target="_blank">@erikras</a>), but has since seen many contributions
-          from the open source community. Thank you to <a
-            href="https://github.com/erikras/react-redux-universal-hot-example/graphs/contributors"
-            target="_blank">all the contributors</a>.
-        </p>
-
-        <h3>Mini Bar <span style={{color: '#aaa'}}>(not that kind)</span></h3>
-
-        <p>Hey! You found the mini info bar! The following component is display-only. Note that it shows the same
-          time as the info bar.</p>
-
-        <MiniInfoBar/>
-
-        <h3>Images</h3>
-
-        <p>
-          Psst! Would you like to see a kitten?
-
-          <button className={'btn btn-' + (showKitten ? 'danger' : 'success')}
-                  style={{marginLeft: 50}}
-                  onClick={this.handleToggleKitten}>
-            {showKitten ? 'No! Take it away!' : 'Yes! Please!'}</button>
-        </p>
-
-        {showKitten && <div><img src={kitten}/></div>}
       </div>
     );
   }
